98925234efe77f841a7e78f2a33a6f68f2ec22a0
Daniel <danielBeets@protonmail.com>
2022-08-16 15:06:29 +0200

    remove data and scripts

--- scripts/tokens-uniswap.sh
+++
@@ -1,5 +0,0 @@
-curl \
-  -X POST \
-  -H "Content-Type: application/json" \
-  --data '{ "query": "{ uniswap(id: 1) { exchanges(first: 200, orderBy: ethLiquidity, orderDirection: desc) { tokenAddress tokenSymbol tokenName tokenDecimals } } }" }' \
-  https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap
